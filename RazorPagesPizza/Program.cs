14ed7afad8e6d6e53ea9947247ca04c2a7eb9d4f
Diego Alonso Rubí Salas <43016665+Diegoalru@users.noreply.github.com>
2024-05-09 00:14:29 +0000

    Add EmailSender service for sending confirmation emails

--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesPizza.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using RazorPagesPizza.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +11,10 @@ builder.Services.AddDbContext<RazorPagesPizzaAuth>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
+
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
